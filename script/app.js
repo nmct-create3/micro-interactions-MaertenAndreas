9e0c7c8ca6716a84a0ab6ed40a4d6c154efdd537
Yimura <andreas.maerten@scarlet.be>
2020-10-09 16:53:02 +0200

    Site: Pushing progress

--- script/app.js
+++ script/app.js
@@ -1,9 +1,14 @@
 function handleFloatingLabel() {
-   
+
 }
 
-function handlePasswordSwitcher() {
-    
+const handlePasswordSwitcher = () => {
+    const passwordInput = document.querySelector('.js-password-input');
+    const passwordToggle = document.querySelector('.js-password-toggle');
+
+    passwordToggle.addEventListener('change', (e) => {
+        passwordInput.type = e.currentTarget.checked ? 'text' : 'password';
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
